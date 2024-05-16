eb0d54c98cc4d9323b2fe9070f9c88561ab27d20
Diana Ortiz <al44776@un.edu.mx>
2024-05-15 23:31:15 -0600

    Cambios en el Programa principal

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Persona persona= new Persona("Diana", 24);
+        persona.MostrarInformacion();
+    }
+}
+class Persona {
+    public string Nombre  { get; set; }
+    public int Edad { get; set;}
+
+    public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+
+    public void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}");
+        }
+}
+
+
+
